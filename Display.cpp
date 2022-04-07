42d294b7fef1d0d2f043069142fb4124c2d82182
Roundanround1 <102126609+Roundanround1@users.noreply.github.com>
2022-04-07 17:37:24 -0500

    Create Display.cpp
    Created display functionality

---
+++ Display.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Display.h"
+using namespace std;
+
+void Display()
+{
+    cout << "\n";
+    cout << "  a b c d e f g h \n";
+    cout << "\e[0m1 \e[0;31mR N B Q K B N R         \n";
+    cout << "\e[0m2 \e[0;31mP P P P P P P P         \n";
+    cout << "\e[0m3 * \e[0;31m* \e[0m* \e[0;31m* \e[0m* \e[0;31m* \e[0m* \e[0;31m*         \n";
+    cout << "\e[0m4 \e[0;31m* \e[0m* \e[0;31m* \e[0m* \e[0;31m* \e[0m* \e[0;31m* \e[0m*         \n";
+    cout << "\e[0m5 * \e[0;31m* \e[0m* \e[0;31m* \e[0m* \e[0;31m* \e[0m* \e[0;31m*         \n";
+    cout << "\e[0m6 \e[0;31m* \e[0m* \e[0;31m* \e[0m* \e[0;31m* \e[0m* \e[0;31m* \e[0m*         \n";
+    cout << "\e[0m7 P P P P P P P P         \n";
+    cout << "\e[0m8 R N B K Q B N R         \n";
+    cout << "\n";
+
+    return;
+}
